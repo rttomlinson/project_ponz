c97322f008f4f8d727ac64291a15cdb95402dbc7
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-26 22:38:03 +0000

    Work on recursion

--- models/user.js
+++ models/user.js
@@ -6,84 +6,91 @@ const findOrCreate = require('mongoose-findorcreate');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  passwordHash: {
-    type: String,
-    required: true
-  },
-  parentId: {
-    type: Schema.Types.ObjectId
-  },
-  children: [
-    {
-      type: Schema.Types.ObjectId
-    }
-  ]
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    parentId: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    children: [{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }]
 });
 
 UserSchema.plugin(uniqueValidator);
 UserSchema.plugin(findOrCreate);
 
 UserSchema.virtual('points').get(function(name) {
-  //points!
-  let points;
-  return this.points;
+    //points!
+    let points;
+    return this.points;
 });
 
 UserSchema.virtual('password').set(function(value) {
-  this.passwordHash = bcrypt.hashSync(value, 8);
+    this.passwordHash = bcrypt.hashSync(value, 8);
 });
 
 UserSchema.virtual('referralLink')
-  .get(function() {
-    let link = `register/${this.id}`;
-    return link;
-  })
-  .set(function(value) {
-    this.referralHash = bcrypt.hashSync(value, 8);
-  });
+    .get(function() {
+        let link = `register/${this.id}`;
+        return link;
+    })
+    .set(function(value) {
+        this.referralHash = bcrypt.hashSync(value, 8);
+    });
 
 UserSchema.methods.validatePassword = function(password) {
-  return bcrypt.compareSync(password, this.passwordHash);
+    return bcrypt.compareSync(password, this.passwordHash);
 };
 
-// UserSchema.methods.populateChildren = function() {
-//   console.log('BEGINNING OF POPULATE CHILDREN');
-//   //returns an array of promises
-//   return Promise.all(
-//     this.children.map(function(childId) {
-//       console.log('childId of user is', childId);
-//       return User.findById(childId).then(child => {
-//         console.log('found child is', child);
-//         if (child.children.length == 0) {
-//           return child;
-//         } else {
-//           return Promise.all(child.populateChildren());
-//         }
-//       });
-//     })
-//   ).catch(function(err) {
-//     console.log(err);
-//   });
-// };
+UserSchema.methods.populateChildren = function() {
+    console.log('BEGINNING OF POPULATE CHILDREN');
+    //returns an array of promises
+    return this.populate('children');
 
-UserSchema.methods.populateChildren = async function() {
-  let children = [];
 
-  this.children.forEach(function(childId) {
+    // return Promise.all(
+    //         this.children.map(function(childId) {
+    //             console.log('childId of user is', childId);
+    //             return User.findById(childId).then(child => {
+    //                 console.log('found child is', child);
+    //                 if (child.children.length == 0) {
+    //                     return child;
+    //                 }
+    //                 else {
+    //                     return child.populateChildren();
+    //                 }
+    //             });
+    //         })
+    //     )
+    //     .then((children) => {
+    //         this.children = children;
+    //         return this;
+    //     }).catch(function(err) {
+    //         console.log(err);
+    //     });
+};
 
-    User.findById(childId).then(child => {
-      let newChild = await child.populateChildren();
-      children.push(newChild);
-    })
-  })
+// UserSchema.methods.populateChildren = async function() {
+//     let children = [];
 
-  return Promise.all(children);
-}
+//     return Promise.all(this.children.map(function(childId) {
+
+//         return User.findById(childId)
+//         .then(child => {
+//             let newChild = await child.populateChildren();
+//             children.push(newChild);
+//         })
+//     }));
+// }
 
 const User = mongoose.model('User', UserSchema);
 
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const {
-  loggedInOnly,
-  loggedOutOnly
+    loggedInOnly,
+    loggedOutOnly
 } = require('../services/session');
 const User = require('../models/').User;
 
 router.get('/', loggedInOnly, async function(req, res, next) {
-  let children = await req.user.populateChildren();
-  console.log('Children: ', children);
+    let children = await req.user.populateChildren();
+    console.log('Children: ', children);
 
-  res.render('home');
+    res.render('home');
 });
 
 router.get('/login', loggedOutOnly, function(req, res) {
-  res.render('login');
+    res.render('login');
 });
 
 router.get('/logout', function(req, res) {
-  req.logout();
-  res.redirect('/');
+    req.logout();
+    res.redirect('/');
 });
 
 module.exports = router;
