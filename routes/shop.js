d860ab93710aa6931192b208dcb4604646fa70d3
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-27 17:06:49 +0000

    Add shop population function

--- routes/shop.js
+++ routes/shop.js
@@ -1,41 +1,55 @@
 const express = require('express');
 const router = express.Router();
 const {
-  loggedInOnly,
-  loggedOutOnly
+    loggedInOnly,
+    loggedOutOnly
 } = require('../services/session');
 const User = require('../models/').User;
 const Item = require('../models/').Item;
 
 router.get('/', loggedInOnly, async function(req, res, next) {
-  res.render('shop', {
-    user
-  });
+    //grab all inventory items then pass to template
+    //let items = Item.getAllItems();
+    
+    // [{
+    //   price:
+    //   imgLocation:
+    // }]
+    
+    
+
+    res.render('shop'/*, {items}*/);
 });
 
-router.post('/buy', loggedInOnly, (req, res) => {
-  let itemId = req.body.itemId;
-  let cost = req.body.cost;
+router.post('/buy:id', loggedInOnly, (req, res) => {
+    let itemId = req.params.id;
+    let cost = req.body.cost;
 
-  User.findByIdAndUpdate(currentUser.id, {
-    dogeCoins: { $inc: cost * -1 }
-  })
-    .then(user => {
-      let itemToIncr = user.inventory.find(function(item) {
-        return item.id == itemId;
-      });
+    User.findByIdAndUpdate(currentUser.id, {
+            dogeCoins: {
+                $inc: cost * -1
+            }
+        })
+        .then(user => {
+            let itemToIncr = user.inventory.find(function(item) {
+                return item.id == itemId;
+            });
 
-      if (itemToIncr) {
-        itemToIncr.quantity++;
-      } else {
-        user.inventory.push({ id: itemId, quantity: 1 });
-      }
+            if (itemToIncr) {
+                itemToIncr.quantity++;
+            }
+            else {
+                user.inventory.push({
+                    id: itemId,
+                    quantity: 1
+                });
+            }
 
-      return user.save();
-    })
-    .then(() => {
-      res.redirect('/');
-    });
+            return user.save();
+        })
+        .then(() => {
+            res.redirect('/');
+        });
 });
 
 module.exports = router;
